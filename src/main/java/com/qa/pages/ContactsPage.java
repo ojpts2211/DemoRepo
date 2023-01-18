ca36647089e36719d880ae99a91c2200e4fc6a1a
Omkar <info.ojpts@yahoo.com>
2023-01-18 17:15:42 +0530

    added new Page as SearchPage

---
+++ src/main/java/com/qa/pages/ContactsPage.java
@@ -0,0 +1,16 @@
+package com.qa.pages;
+
+public class ContactsPage {
+
+	
+	
+	
+	public void contactsPage() {
+		System.out.println("New Contacts Added");
+	}
+	
+	public void searchPage() {
+		System.out.println("Searching Omkar Jagdale");
+	}
+	
+}
